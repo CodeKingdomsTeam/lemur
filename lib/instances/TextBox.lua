3ea7923ecf5ab6b121ac8e7514ecfa8d22cdf7ad
Hugheth <thehugheth@gmail.com>
2019-08-28 15:47:57 +0100

    Add TextEditable to TextBox

--- lib/instances/TextBox.lua
+++ lib/instances/TextBox.lua
@@ -7,98 +7,170 @@ local TextTruncate = import("../Enum/TextTruncate")
 local TextXAlignment = import("../Enum/TextXAlignment")
 local TextYAlignment = import("../Enum/TextYAlignment")
 
-local TextBox = GuiObject:extend("TextBox", {
-	creatable = true,
-})
+local TextBox =
+	GuiObject:extend(
+	"TextBox",
+	{
+		creatable = true
+	}
+)
 
-TextBox.properties.ClearTextOnFocus = InstanceProperty.typed("boolean", {
-	getDefault = function()
-		return true
-	end,
-})
+TextBox.properties.ClearTextOnFocus =
+	InstanceProperty.typed(
+	"boolean",
+	{
+		getDefault = function()
+			return true
+		end
+	}
+)
 
-TextBox.properties.FocusLost = InstanceProperty.readOnly({
-	getDefault = function()
-		return Signal.new()
-	end,
-})
+TextBox.properties.FocusLost =
+	InstanceProperty.readOnly(
+	{
+		getDefault = function()
+			return Signal.new()
+		end
+	}
+)
 
-TextBox.properties.Focused = InstanceProperty.readOnly({
-	getDefault = function()
-		return Signal.new()
-	end,
-})
+TextBox.properties.Focused =
+	InstanceProperty.readOnly(
+	{
+		getDefault = function()
+			return Signal.new()
+		end
+	}
+)
 
-TextBox.properties.Font = InstanceProperty.enum(Font, {
-	getDefault = function()
-		return Font.SourceSans
-	end,
-})
+TextBox.properties.Font =
+	InstanceProperty.enum(
+	Font,
+	{
+		getDefault = function()
+			return Font.SourceSans
+		end
+	}
+)
 
-TextBox.properties.PlaceholderText = InstanceProperty.typed("string", {
-	getDefault = function()
-		return ""
-	end,
-})
+TextBox.properties.PlaceholderText =
+	InstanceProperty.typed(
+	"string",
+	{
+		getDefault = function()
+			return ""
+		end
+	}
+)
 
-TextBox.properties.ShowNativeInput = InstanceProperty.typed("boolean", {
-	getDefault = function()
-		return true
-	end,
-})
+TextBox.properties.ShowNativeInput =
+	InstanceProperty.typed(
+	"boolean",
+	{
+		getDefault = function()
+			return true
+		end
+	}
+)
 
-TextBox.properties.Text = InstanceProperty.typed("string", {
-	getDefault = function()
-		return "TextBox"
-	end,
-})
+TextBox.properties.Text =
+	InstanceProperty.typed(
+	"string",
+	{
+		getDefault = function()
+			return "TextBox"
+		end
+	}
+)
 
-TextBox.properties.TextColor3 = InstanceProperty.typed("Color3", {
-	getDefault = function()
-		return Color3.new()
-	end,
-})
+TextBox.properties.TextColor3 =
+	InstanceProperty.typed(
+	"Color3",
+	{
+		getDefault = function()
+			return Color3.new()
+		end
+	}
+)
 
-TextBox.properties.TextSize = InstanceProperty.typed("number", {
-	getDefault = function()
-		return 14
-	end,
-})
+TextBox.properties.TextSize =
+	InstanceProperty.typed(
+	"number",
+	{
+		getDefault = function()
+			return 14
+		end
+	}
+)
 
-TextBox.properties.TextTransparency = InstanceProperty.typed("number", {
-	getDefault = function()
-		return 0
-	end,
-})
+TextBox.properties.TextTransparency =
+	InstanceProperty.typed(
+	"number",
+	{
+		getDefault = function()
+			return 0
+		end
+	}
+)
 
-TextBox.properties.TextTruncate = InstanceProperty.enum(TextTruncate, {
-	getDefault = function()
-		return TextTruncate.None
-	end,
-})
+TextBox.properties.TextTruncate =
+	InstanceProperty.enum(
+	TextTruncate,
+	{
+		getDefault = function()
+			return TextTruncate.None
+		end
+	}
+)
 
-TextBox.properties.TextWrapped = InstanceProperty.typed("boolean", {
-	getDefault = function()
-		return false
-	end,
-})
+TextBox.properties.TextWrapped =
+	InstanceProperty.typed(
+	"boolean",
+	{
+		getDefault = function()
+			return false
+		end
+	}
+)
 
-TextBox.properties.TextScaled = InstanceProperty.typed("boolean", {
-	getDefault = function()
-		return false
-	end,
-})
+TextBox.properties.TextScaled =
+	InstanceProperty.typed(
+	"boolean",
+	{
+		getDefault = function()
+			return false
+		end
+	}
+)
 
-TextBox.properties.TextXAlignment = InstanceProperty.enum(TextXAlignment, {
-	getDefault = function()
-		return TextXAlignment.Center
-	end,
-})
+TextBox.properties.TextXAlignment =
+	InstanceProperty.enum(
+	TextXAlignment,
+	{
+		getDefault = function()
+			return TextXAlignment.Center
+		end
+	}
+)
 
-TextBox.properties.TextYAlignment = InstanceProperty.enum(TextYAlignment, {
-	getDefault = function()
-		return TextYAlignment.Center
-	end,
-})
+TextBox.properties.TextYAlignment =
+	InstanceProperty.enum(
+	TextYAlignment,
+	{
+		getDefault = function()
+			return TextYAlignment.Center
+		end
+	}
+)
+
+TextBox.properties.TextEditable =
+	InstanceProperty.typed(
+	"boolean",
+	{
+		getDefault = function()
+			return false
+		end
+	}
+)
 
 return TextBox
